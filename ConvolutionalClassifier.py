9aae3d6bc7dbe408853461f271cc8633a1454c7b
Ali Nour <56680303+Ali-12122@users.noreply.github.com>
2022-09-21 11:13:17 +0200

    Add files via upload

--- ConvolutionalClassifier.py
+++ ConvolutionalClassifier.py
@@ -1,44 +1,29 @@
+import torch
 import torch.nn as nn
 
 
 class ConvolutionalClassifier(nn.Module):
-    def __init__(self, classifier_input_channels, number_of_classes,
-                 model_dimensions_of_convolution, activation_function=nn.ReLU(inplace=True)):
+    def __init__(self, classifier_input_size, number_of_classes, activation_function=nn.ReLU(inplace=True)):
         super(ConvolutionalClassifier, self).__init__()
-
-        self.classifier_input_channels = classifier_input_channels
+        self.classifier_input_size = classifier_input_size
         self.number_of_classes = number_of_classes
-        self.model_dimensions_of_convolution = model_dimensions_of_convolution
         self.activation_function = activation_function
-
-        assert 1 <= model_dimensions_of_convolution <= 3, 'model_dimensions_of_convolution should be between 1 and 3'
-
-        self.convolutional_classifier_1d = nn.Sequential(
-            nn.Conv1d(self.classifier_input_channels, 1024, kernel_size=1), activation_function,
-            nn.Conv1d(1024, 512, kernel_size=1), activation_function,
+        self.convolutional_classifier = nn.Sequential(
+            nn.Conv1d(self.classifier_input_size, 1024, kernel_size=1), activation_function,
+            nn.Conv1d(1024, 768, kernel_size=1), activation_function,
+            nn.Conv1d(768, 512, kernel_size=1), activation_function,
             nn.Conv1d(512, 256, kernel_size=1), activation_function,
             nn.Conv1d(256, 128, kernel_size=1), activation_function,
-            nn.Conv1d(128, number_of_classes, kernel_size=1)
-        )
-        self.convolutional_classifier_2d = nn.Sequential(
-            nn.Conv2d(self.classifier_input_channels, 1024, kernel_size=1), activation_function,
-            nn.Conv2d(1024, 512, kernel_size=1), activation_function,
-            nn.Conv2d(512, 256, kernel_size=1), activation_function,
-            nn.Conv2d(256, 128, kernel_size=1), activation_function,
-            nn.Conv2d(128, number_of_classes, kernel_size=1)
-        )
-        self.convolutional_classifier_3d = nn.Sequential(
-            nn.Conv3d(self.classifier_input_channels, 1024, kernel_size=1), activation_function,
-            nn.Conv3d(1024, 512, kernel_size=1), activation_function,
-            nn.Conv3d(512, 256, kernel_size=1), activation_function,
-            nn.Conv3d(256, 128, kernel_size=1), activation_function,
-            nn.Conv3d(128, number_of_classes, kernel_size=1)
+            nn.Conv1d(128, 64, kernel_size=1), activation_function,
+            nn.Conv1d(64, 32, kernel_size=1), activation_function,
+            nn.Conv1d(32, 16, kernel_size=1), activation_function,
+            nn.Conv1d(16, number_of_classes, kernel_size=1)
         )
 
     def forward(self, input_data):
-        if self.model_dimensions_of_convolution == 1:
-            return self.convolutional_classifier_1d(input_data)
-        elif self.model_dimensions_of_convolution == 2:
-            return self.convolutional_classifier_2d(input_data)
-        else:
-            return self.convolutional_classifier_3d(input_data)
+        data_tensor = torch.flatten(input_data)
+        data_tensor = torch.unsqueeze(data_tensor, 1)
+        data_tensor = torch.unsqueeze(data_tensor, 0)
+        data_tensor = self.convolutional_classifier(data_tensor)
+        prediction_vector = torch.squeeze(data_tensor)
+        return prediction_vector
